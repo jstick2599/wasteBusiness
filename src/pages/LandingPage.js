1931ae1f4ab62a3acacd82f578a4dfbdf54ad830
Joshua <jstick2599@gmail.com>
2025-11-03 14:06:06 -0700

    fixed styling of inputs and started work on file upload

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,6 +8,19 @@ import truckPickupMediumShot from "../images/truckPickupMediumShot.jpg";
 import truckPickupCloseShot from "../images/truckPickupCloseShot.jpg";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import "./LandingPage.css";
+const printFile = (event) => {
+  const files = event.target.files; // get selected files
+
+  if (files.length === 0) {
+    console.log("No file selected.");
+    return;
+  }
+
+  // Print file names
+  for (let i = 0; i < files.length; i++) {
+    console.log("File name:", files[i].name);
+  }
+};
 
 // Slideshow component remains unchanged
 const Slideshow = ({ imgs }) => {
@@ -82,31 +95,44 @@ const LandingPage = () => {
         ]}
       />
       <div>
-        <h1>Junk Removal Quote Request Form
-Get a Free Quote, don't forget to include pictures or a detailed description of what junk you need removed.</h1>
+        <h1 className="formDescription">Junk Removal Quote Request Form</h1>
 
         <form action="/" className="userForm">
           <div className="namePhoneDiv">
-            <label htmlFor="nameInput">Name</label>
+            {/* <label htmlFor="nameInput">Name</label> */}
             <input type="text" id="nameInput" name="fname" placeholder="Name" className="name" />
-            <label htmlFor="phoneInput">Phone Number</label>
+            {/* <label htmlFor="phoneInput">Phone Number</label> */}
             <input type="text" id="phoneInput" name="phone" placeholder="Phone Number" />
           </div>
           <div className="emailZipDiv">
-            <label htmlFor="emailInput">E-mail</label>
+            {/* <label htmlFor="emailInput">E-mail</label> */}
             <input type="text" id="emailInput" name="email" placeholder="E-mail" className="email" />
-            <label htmlFor="zipInput">Zip Code</label>
+            {/* <label htmlFor="zipInput">Zip Code</label> */}
             <input type="text" id="zipInput" name="zip" placeholder="Zip Code" />
           </div>
           <div className="trashDescriptionAndSubmit">
             <label htmlFor="descriptionInput">What do you need to have removed? How quickly does it need to be removed?</label>
-            <textarea id="descriptionInput" name="description" placeholder="What do you need to have removed? How quickly does it need to be removed?"/>
-            <input type="submit" value="Submit" />
+            <textarea id="descriptionInput" name="description" />
+            
           </div>
+          <div className="fileUploadContainer">
+            <label htmlFor="fileInput" className="uploadButton">
+              📸 Upload Pictures for Accurate Pricing
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              name="files"
+              accept="image/*"
+              multiple
+              onChange={printFile}
+            />
+
+          </div>
+
         </form>
 
-        <p>Click the "Submit" button and the form-data will be sent to a page on the
-        server called "action_page.php".</p>
+        <p>Click the "Submit" button to get a Free Quote, don't forget to include pictures or a detailed description of what junk you need removed.</p>
       </div>
     </div>
   );
